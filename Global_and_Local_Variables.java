a77243ffe801d1a1ded4984ec5daacdcb0caf1bb
Sahu-Chitransh <chitranshsahukwi@gmail.com>
2024-07-29 15:28:15 +0530

    Add files via upload

---
+++ Global_and_Local_Variables.java
@@ -0,0 +1,7 @@
+public class Global_and_Local_Variables {
+    int a = 55;
+    public static void main(String[] args) {
+        int a = 12;
+        System.out.println("The Value of a is - "+a);
+    }    
+}
