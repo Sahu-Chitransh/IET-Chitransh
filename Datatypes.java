c7a705cf9db750ff757e9bb35697ea8f88f60ad8
Sahu-Chitransh <chitranshsahukwi@gmail.com>
2024-07-29 15:19:57 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,15 @@
+public class Datatypes{
+    public static void main(String args[]){
+        int a = 89;
+        short s = 19855;
+        long l = 985217654;
+        byte b = 123;
+        char c = 'C';
+        
+        System.out.println("Integer Datatype - " + a);
+        System.out.println("Short Datatype - " + s);
+        System.out.println("Byte  Datatype - " + b);
+        System.out.println("Long  Datatype - " + l);
+        System.out.println("Character Datatype - " + c);
+    }
+}
